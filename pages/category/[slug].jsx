096657117ac27f1868e2e053f095d51834204816
saikiranAnnam <saikiranannam23@gmail.com>
2023-06-18 12:16:21 +0530

    solving : Error: Invalid  value returned from getStaticPaths in /category/[slug].

--- pages/category/[slug].jsx
+++ pages/category/[slug].jsx
@@ -92,16 +92,16 @@ export default Category;
 export async function getStaticPaths() {
     const category = await fetchDataFromApi("/api/categories?populate=*");
     const paths = category?.data?.map((c) => ({
-        params: {
-            slug: c.attributes.slug
-        },
+      params: {
+        slug: String(c.attributes.slug),
+      },
     }));
-
+  
     return {
-        paths,
-        fallback: false,
+      paths,
+      fallback: false,
     };
-}
+  }
 
 // `getStaticPaths` requires using `getStaticProps`
 export async function getStaticProps({ params: { slug } }) {
