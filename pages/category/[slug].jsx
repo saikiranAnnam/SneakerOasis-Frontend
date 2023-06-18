d310b5d0bfd8eb578d9638e9d13305459638d98c
saikiranAnnam <saikiranannam23@gmail.com>
2023-06-18 13:28:18 +0530

    fixing error while deploying

--- pages/category/[slug].jsx
+++ pages/category/[slug].jsx
@@ -89,15 +89,17 @@ const Category = ({ category, products, slug }) => {
 
 export default Category;
 
+
 export async function getStaticPaths() {
     try{
-    const category = await fetchDataFromApi("api/categories?populate=*");
-    const paths = category?.data?.map((c) => String(({
+    const category = await fetchDataFromApi("/api/categories?populate=*");
+    const paths = category?.data?.map((c) => ({
       params: {
         slug: c.attributes.slug,
       },
-    })));
-        console.log("paths:", paths);
+    }));
+    
+  
     return {
       paths,
       fallback: false,
@@ -114,7 +116,7 @@ export async function getStaticPaths() {
   }
 
 // `getStaticPaths` requires using `getStaticProps`
-export async function getStaticProps({ params: { slug } }) {
+export async function getStaticProps({ params:  {slug} }) {
     const category = await fetchDataFromApi(
         `/api/categories?filters[slug][$eq]=${slug}`
     );
--- utils/api.js
+++ utils/api.js
@@ -1,33 +1,28 @@
 import { API_URL, STRAPI_API_TOKEN } from "./urls";
 
-
-// fetches all products 
 export const fetchDataFromApi = async (endpoint) => {
-  const options = {
-      method: "GET",
-      headers: {
-          Authorization: "Bearer " + STRAPI_API_TOKEN,
-      },
-  };
+    const options = {
+        method: "GET",
+        headers: {
+            Authorization: "Bearer " + STRAPI_API_TOKEN,
+        },
+    };
 
-  const res = await fetch(`${API_URL}${endpoint}`, options);
-  const data = await res.json();
+    const res = await fetch(`${API_URL}${endpoint}`, options);
+    const data = await res.json();
 
-  return data;
+    return data;
 };
 
-
-// make payment
-
 export const makePaymentRequest = async (endpoint, payload) => {
-  const res = await fetch(`${API_URL}${endpoint}`, {
-      method: "POST",
-      headers: {
-          Authorization: "Bearer " + STRAPI_API_TOKEN,
-          "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-  });
-  const data = await res.json();
-  return data;
+    const res = await fetch(`${API_URL}${endpoint}`, {
+        method: "POST",
+        headers: {
+            Authorization: "Bearer " + STRAPI_API_TOKEN,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    });
+    const data = await res.json();
+    return data;
 };
