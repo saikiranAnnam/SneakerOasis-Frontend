ae68de76eabef20c7c416dcc375ba426ec6231e1
saikiranAnnam <saikiranannam23@gmail.com>
2023-06-18 15:25:30 +0530

    solving deployment error 7

--- pages/category/[slug].jsx
+++ pages/category/[slug].jsx
@@ -93,7 +93,7 @@ export default Category;
 export async function getStaticPaths() {
     try{
     const category = await fetchDataFromApi("/api/categories?populate=*");
-    const paths = category.data.map((c) => ({
+    const paths = category?.data?.map((c) => ({
       params: {
         slug: c.attributes.slug,
       },
