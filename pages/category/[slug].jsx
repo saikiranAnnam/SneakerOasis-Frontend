4caa45b1b9856b323f65d45a4fde58f7fbbcb008
saikiranAnnam <saikiranannam23@gmail.com>
2023-06-19 14:58:54 +0530

    solving deployment error 11

--- pages/category/[slug].jsx
+++ pages/category/[slug].jsx
@@ -91,38 +91,27 @@ export default Category;
 
 
 export async function getStaticPaths() {
-    try{
     const category = await fetchDataFromApi("/api/categories?populate=*");
-    const paths = category?.data?.map((c) =>
-    {
-        return{
-            params: { slug : c.attributes.slug.toString() }
-        }
-    });  
-    return {
-      paths,
-      fallback: true,
-    };
+    const paths = category?.data?.map((c) => ({
+        params: {
+            slug: c.attributes.slug,
+        },
+    }));
 
-    }catch(error){
-    console.error("Error fetching category data:", error);
     return {
-      paths: [],
-      fallback: false,
+        paths,
+        fallback: false,
     };
-    }
-    
-  }
+}
 
 // `getStaticPaths` requires using `getStaticProps`
-export async function getStaticProps({ params:  {slug} }) {
+export async function getStaticProps({ params: { slug } }) {
     const category = await fetchDataFromApi(
         `/api/categories?filters[slug][$eq]=${slug}`
     );
     const products = await fetchDataFromApi(
         `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=${maxResult}`
     );
-    
 
     return {
         props: {
